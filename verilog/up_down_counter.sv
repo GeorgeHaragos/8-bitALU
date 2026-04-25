07374522ef29e9e7b8c0aa666af788d666bde04d
George Haragos <george-andrei.haragos@student.upt.ro>
2026-04-25 12:56:26 +0300

    Update up_down_counter.sv

--- verilog/up_down_counter.sv
+++ verilog/up_down_counter.sv
@@ -20,6 +20,7 @@ module up_down_counter(
   input clk,
   input rst,
   input M,
+  input en,
   output [2:0]Q
   );
   wire M_bar = ~M;
@@ -27,18 +28,19 @@ module up_down_counter(
   wire up_2, down_2;
   wire [2:0]T;
   
-  assign T[0]=1'b1;
+  assign T[0]=en;
   
   assign up_1 = M & Q[0];
   assign down_1 = M_bar & ~Q[0];
-  assign T[1] = up_1 | down_1;
+  assign T[1] = en & (up_1 | down_1);
   
   assign up_2 = up_1 & Q[1];
   assign down_2 = down_1 & ~Q[1];
-  assign T[2] = up_2 | down_2;
+  assign T[2] = en & (up_2 | down_2);
   
   t_ff FF1(.clk(clk),.r(rst),.t(T[0]),.q(Q[0]),.q_barat());
   t_ff FF2(.clk(clk),.r(rst),.t(T[1]),.q(Q[1]),.q_barat());
   t_ff FF3(.clk(clk),.r(rst),.t(T[2]),.q(Q[2]),.q_barat());
   
 endmodule
+
